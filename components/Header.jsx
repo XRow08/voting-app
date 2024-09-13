cf216c7053173d71c9d8782fd85e286a001485a1
Pedro Pimenta <znzdesign@gmail.com>
2024-09-13 23:43:35 +0100

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,35 +3,36 @@ import Image from "next/image";
 import StarSwitch from "./Starswitch";
 
 const Header = () => {
+  const imageProps = {
+    width: 44,
+    height: 44,
+    quality: 100,
+    priority: true
+  };
+
   return (
     <header className="flex justify-between items-center">
-          <div className="flex items-center w-1/2">
-            <span className="bg-orange-500 text-white p-4 py-1 flex items-left">
-              <StarSwitch />
-            </span>
-          </div>          
-          <div className="flex space-x-2 pr-4 pt-3 items-center">
-          <Link href="/">
+      <div className="flex items-center w-1/2">
+        <span className="bg-orange-500 text-white p-4 py-1 flex items-left">
+          <StarSwitch />
+        </span>
+      </div>          
+      <div className="flex space-x-2 pr-4 pt-3 items-center">
+        <Link href="/">
           <Image
             src="/assets/stars-off/question-icon.png"
             alt="Question Icon"
-            width={44}
-            height={44}
-            quality={100}
-            priority
+            {...imageProps}
           />
-          </Link>
-          <Link href="/">
-            <Image
-              src="/assets/stars-off/x-icon.png"
-              alt="Close"
-              width={44}
-              height={44}
-              quality={100}
-              priority
-            />
-          </Link>
-        </div>
+        </Link>
+        <Link href="/">
+          <Image
+            src="/assets/stars-off/x-icon.png"
+            alt="Close"
+            {...imageProps}
+          />
+        </Link>
+      </div>
     </header>
   )
 };
