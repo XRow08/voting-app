3e020659ca6e117beef0e85e5a4a4a24d0b94512
Pedro Pimenta <znzdesign@gmail.com>
2024-09-18 01:58:48 +0100

    Update YourVotes.jsx

--- components/YourVotes.jsx
+++ components/YourVotes.jsx
@@ -1,33 +1,32 @@
-// create component for your votes
 import Image from 'next/image';
 
 export default function YourVotes({ userVotes }) {
     return (
-        <div className="flex justify-between items-center w-full mb-8 mt-4">
-        <div className="flex items-center">
-          <div className="relative mr-2">
-            <Image
-              src="/assets/stars-off/animation-bg.png"
-              alt="Your Votes Background"
-              width={50}
-              height={50}
-            />
-            <div className="absolute inset-0 flex items-center justify-center">
-              <Image
-                src="/assets/stars-off/hands-animation.gif"
-                alt="Hands Animation"
-                width={30}
-                height={30}
-              />
+        <div className="flex justify-between items-center w-full mb-8 mt-4 bg-[#1E1E1E] rounded-full p-2">
+            <div className="flex items-center">
+                <div className="relative mr-3">
+                    <Image
+                        src="/assets/stars-off/animation-bg.png"
+                        alt="Your Votes Background"
+                        width={40}
+                        height={40}
+                    />
+                    <div className="absolute inset-0 flex items-center justify-center">
+                        <Image
+                            src="/assets/stars-off/hands-animation.gif"
+                            alt="Hands Animation"
+                            width={24}
+                            height={24}
+                        />
+                    </div>
+                </div>
+                <span className="text-white font-semibold text-sm">Your Votes</span>
             </div>
-          </div>
-          <span className="text-white font-bold text-sm">Your Votes</span>
+            <div className="bg-[#2C2C2C] rounded-full p-1">
+                <div className="bg-[#1E1E1E] rounded-full w-8 h-8 flex items-center justify-center">
+                    <span className="text-white font-bold text-sm">{userVotes}</span>
+                </div>
+            </div>
         </div>
-        <div className="bg-white rounded-full p-1">
-          <div className="bg-black rounded-full w-8 h-8 flex items-center justify-center">
-            <span className="text-white font-bold text-sm">{userVotes}</span>
-          </div>
-        </div>
-      </div>
     );
 }
