70c913f602e3ec309bec2b515b9bac4204ac3924
Pedro Pimenta <znzdesign@gmail.com>
2024-09-24 00:21:52 +0100

    Update StartingPage.jsx

--- components/StartingPage.jsx
+++ components/StartingPage.jsx
@@ -1,8 +1,13 @@
 'use client';
 
+
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
+import Link from 'next/link';
+
+
+
 
 export default function StartingPage() {
   const router = useRouter();
@@ -24,26 +29,13 @@ export default function StartingPage() {
   }, [router]);
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black">
-      <div className="relative">
-        <Image
-          src="/assets/stars-off/animation-bg.png"
-          alt="Animation Background"
-          width={150}
-          height={150}
-        />
-        <div className="absolute inset-0 flex items-center justify-center">
-          <Image
-            src="/assets/stars-off/hands-animation.gif"
-            alt="Hands Animation"
-            width={100}
-            height={100}
-          />
+    <html lang="en">
+      <body>
+        <div className="absolute bottom-10 text-white text-2xl">
+          Redirecionando em {countdown}...
         </div>
-      </div>
-      <div className="absolute bottom-10 text-white text-2xl">
-        Redirecionando em {countdown}...
-      </div>
-    </div>
+      </body>
+    </html>
   );
+  
 }
